f15566850242c8a942b27aba1699428da571269e
Your Name <you@example.com>
2023-08-23 04:34:33 -0500

    format: cmd terminal output into 2d array

--- src/components/windows/CmdPrompt.jsx
+++ src/components/windows/CmdPrompt.jsx
@@ -9,14 +9,17 @@ export default function CmdPrompt({ isOpen, setIsOpen }) {
     const inputRef = useRef(null);
     const outputRef = useRef(null);
     const [prompt, setPrompt] = useState('C:&#92;WINDOWS&#92;SYSTEM32&gt;&nbsp;')
+    const [scenarioIdx, setScenarioIdx] = useState(0)
 
-    let terminalOutput = [
-        `Microsoft&#10094;R&#10095; Windows DOS &#10094;C&#10095; Copyright Microsoft Corp 1990-2001.\n`,
-        prompt, `type C:\\lulz.txt\n\n`,
-        'J00 |-|4V3 833|\\| |-|4(|<3|) 4|\\||) |>VV|\\|3|) 4|\\||) |_|773.-|_`/ 4|\\||\\||-|1|_473|) 8`/ 73|-| 9.-347357 |\\/|057 |_337 |-|4><0.- 0f 4|_|_ 71|\\/|3 J3Ff |<\nif joo w4N7 7o 54ve joor pREcIouZ FILEz, JOO wILL h4VE 7o 57OP my POpuPZ fir57.\n\n'
+    let outputScenarios = [
+        [
+            `Microsoft&#10094;R&#10095; Windows DOS &#10094;C&#10095; Copyright Microsoft Corp 1990-2001.\n`,
+            prompt, `type C:\\lulz.txt\n\n`,
+            'J00 |-|4V3 833|\\| |-|4(|<3|) 4|\\||) |>VV|\\|3|) 4|\\||) |_|773.-|_`/ 4|\\||\\||-|1|_473|) 8`/ 73|-| 9.-347357 |\\/|057 |_337 |-|4><0.- 0f 4|_|_ 71|\\/|3 J3Ff |<\nif joo w4N7 7o 54ve joor pREcIouZ FILEz, JOO wILL h4VE 7o 57OP my POpuPZ fir57.\n\n'
+        ]
     ]
 
-    const [output, setOutput] = useState(terminalOutput)
+    const [output, setOutput] = useState(outputScenarios[scenarioIdx])
 
     const windowSize = { x: 600, y: 300 }
     const windowOptions = {
@@ -48,21 +51,21 @@ export default function CmdPrompt({ isOpen, setIsOpen }) {
                 {...commonWindowProps}
             >
                 <pre ref={outputRef}
-                    onClick={()=>inputRef.current.focus()}
+                    onClick={() => inputRef.current.focus()}
                 >
                     <div>
                         {output.map((line, index) => (
                             <span key={index} dangerouslySetInnerHTML={{ __html: line }} />
                         ))}
                         <span>
-                            C:&#92;WINDOWS&#92;SYSTEM32&gt;&nbsp;
+                            <span dangerouslySetInnerHTML={{__html: prompt}} />
                             <input
                                 ref={inputRef}
                                 onKeyDown={(e) => {
                                     if (e.key === 'Enter') {
                                         const newLine = prompt + e.target.value + '\n';
-                                        setOutput([...output, newLine]); 
-                                        e.target.value = ''; 
+                                        setOutput([...output, newLine]);
+                                        e.target.value = '';
                                         console.log(output)
                                         outputRef.current.scrollTop = outputRef.current.scrollHeight;
                                     }
