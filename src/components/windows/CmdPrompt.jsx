53f1fab8d955054f597ad0013cff784a082d79d9
Your Name <you@example.com>
2023-08-24 01:03:04 -0500

    fix: failed to fix relative path traversal

--- src/components/windows/CmdPrompt.jsx
+++ src/components/windows/CmdPrompt.jsx
@@ -10,7 +10,7 @@ export default function CmdPrompt({ isOpen, setIsOpen }) {
     const inputRef = useRef(null);
     const outputRef = useRef(null);
     // const [prompt, setPrompt] = useState('C:&#92;WINDOWS&#92;SYSTEM32&gt;&nbsp;')
-    const [prompt, setPrompt] = useState('C:\\WINDOWS\\SYSTEM32'+ '> ')
+    const [prompt, setPrompt] = useState('C:\\WINDOWS\\SYSTEM32' + '> ')
     const [scenarioIdx, setScenarioIdx] = useState(0)
 
     let outputScenarios = [
@@ -70,36 +70,110 @@ export default function CmdPrompt({ isOpen, setIsOpen }) {
         return null;
     }
 
-    function runCdCmd(args) {
-        let destination
-        if (args[0]) destination = args[0].toLowerCase()
-        else return []
+    // function runCdCmd(args) {
+    //     let destination
+    //     if (args[0]) destination = args[0].toLowerCase()
+    //     else return []
 
-        const currentDirectory = prompt.split('> ')[0];
+    //     const currentDirectory = prompt.split('> ')[0];
+
+    //     if (!destination || destination === '.') {
+    //         // No destination provided or just ".": stay in the same directory
+    //         return [prompt + ' '];
+    //     } else if (destination === '..') {
+    //         // Go to the parent directory
+    //         let parentDirectory = currentDirectory.split('\\').slice(0, -1).join('\\');
+    //         // Add trailing file sep at root depth
+    //         if (parentDirectory.length === 2) {
+    //             parentDirectory += '\\'
+    //         }
+    //         // Prevent parent traversal beyond root
+    //         else if (parentDirectory.length <= 1) return []
+    //         setPrompt(parentDirectory.toUpperCase() + '> ');
+    //         return [];
+    //     } else if (existingDirectories.includes(destination)) {
+    //         // Navigate to an existing directory
+    //         setPrompt(destination.toUpperCase() + '> ');
+    //         return [];
+    //     } else {
+    //         // Check for relative path
+    //         const existingDir = findExistingDirectory(destination);
+    //         if (existingDir) {
+    //             setPrompt(existingDir.toUpperCase() + '> ');
+    //             return [];
+    //         } else {
+    //             // Directory doesn't exist
+    //             return [`The system cannot find the path specified.`];
+    //         }
+    //     }
+    // }
+    function runCdCmd(args) {
+
+        const normalizePath = (path) => {
+            if (path.endsWith('> ')) {
+                path = path.split('> ')[0]
+            }
+            return path.toLowerCase();
+        }
+
+        let destination;
+        if (args[0]) {
+            destination = normalizePath(args[0]);
+        }
+        else {
+            return [];
+        }
+        let currentDirectory = normalizePath(prompt);
+        let parentDirectory = currentDirectory.split('\\').slice(0, -1).join('\\');
+
+        console.log("destination", destination)
+        console.log("currentDirectory", currentDirectory)
+        console.log("parentDirectory", parentDirectory)
+
+        const updatePrompt = (path) => setPrompt(path.toUpperCase() + "> ")
 
         if (!destination || destination === '.') {
             // No destination provided or just ".": stay in the same directory
             return [prompt + ' '];
         } else if (destination === '..') {
             // Go to the parent directory
-            let parentDirectory = currentDirectory.split('\\').slice(0, -1).join('\\');
+            // let parentDirectory = currentDirectory.split('\\').slice(0, -1).join('\\');
             // Add trailing file sep at root depth
             if (parentDirectory.length === 2) {
-                parentDirectory += '\\'
+                parentDirectory += '\\';
             }
             // Prevent parent traversal beyond root
-            else if (parentDirectory.length <= 1 ) return []
-            setPrompt(parentDirectory.toUpperCase() + '> ');
+            else if (parentDirectory.length <= 1) return [];
+            // setPrompt(parentDirectory.toUpperCase() + '> ');
+            updatePrompt(parentDirectory)
             return [];
         } else if (existingDirectories.includes(destination)) {
             // Navigate to an existing directory
-            setPrompt(destination.toUpperCase() + '> ');
+            // setPrompt(destination.toUpperCase() + '> ');
+            updatePrompt(destination)
             return [];
         } else {
-            // Check for relative path
-            const existingDir = findExistingDirectory(destination);
-            if (existingDir) {
-                setPrompt(existingDir.toUpperCase() + '> ');
+            // Check for relative path in child directories
+            console.log("currentDirectory", `${currentDirectory}`)
+            if (currentDirectory.endsWith('\\')) {
+                currentDirectory = currentDirectory.split('\\')[0];
+            }
+            // const childPaths = existingDirectories.filter(dir => dir.startsWith(`${currentDirectory}\\`))
+            const childPaths = existingDirectories.filter(dir => dir.startsWith(`${currentDirectory}\\`))
+            // const matchingChildDir = childPaths.filter(dir => dir.endsWith(`\\${destination}`))
+            const matchingChildDir = childPaths.filter(dir => dir.endsWith(`\\${destination}`))
+            console.log("childPaths", childPaths)
+            console.log("matchingChildDir", matchingChildDir)
+            if (matchingChildDir.length === 1) {
+                console.log("parentDirectory", parentDirectory)
+                console.log("currentDirectory.split(parentDirectory)", currentDirectory.split(parentDirectory).slice(1)[0])
+                console.log("matchingChildDir[0].split('\\').slice(-2)[0]", matchingChildDir[0].split('\\').slice(-2)[0])
+                if (matchingChildDir[0].split('\\').slice(-2)[0] == currentDirectory) {
+                    updatePrompt(matchingChildDir[0])
+                    // setPrompt(matchingChildDir[0].toUpperCase() + '> ');
+                } else {
+                    return [`The system cannot find the path specified.`]
+                }
                 return [];
             } else {
                 // Directory doesn't exist
@@ -109,20 +183,29 @@ export default function CmdPrompt({ isOpen, setIsOpen }) {
     }
 
     const processCommand = (stdin) => {
-        const argv = stdin
-            .split(' ')
-            .filter(item => item != '');
+        const argv = stdin.split(' ').filter(item => item !== '');
         const script = argv[0];
         const args = argv.slice(1);
-        let stdout;
-        if (script == 'dir') stdout = runDirCmd(args)
-        else if (script == 'cd') stdout = runCdCmd(args)
-        else if (script) stdout = [`Can't recognize '${script}' as an internal or external command, or batch script.`]
-        else return []
 
-        return stdout.map(each => each + '\n')
+        const stdout = (() => {
+            switch (script) {
+                case 'dir':
+                    return runDirCmd(args);
+                case 'cd':
+                    return runCdCmd(args);
+                default:
+                    if (script) {
+                        return [`Can't recognize '${script}' as an internal or external command, or batch script.`];
+                    } else {
+                        return [];
+                    }
+            }
+        })();
+
+        console.log(stdout)
+
+        return stdout.map(each => each + '\n');
     }
-
     return (
         isOpen && <>
             <Window
